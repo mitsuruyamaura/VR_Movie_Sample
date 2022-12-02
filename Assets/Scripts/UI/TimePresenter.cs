5490dcd3cab16971de3a5b9f2df9c0304ae5e777
mitsuruyamaura <komayou326@gmail.com>
2022-12-02 15:37:00 +0900

    Text と TextMeshPro の両方を用意。通常の View と DOTween 利用時の場合を用意。TimeModel.cs に ReactiveProperty 追加。 TimeView.cs、TimePresenter.cs 作成。

---
+++ Assets/Scripts/UI/TimePresenter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Cysharp.Threading.Tasks;
+
+public class TimePresenter : MonoBehaviour
+{
+    [SerializeField]
+    private TimeModel timeModel;
+
+    [SerializeField]
+    private TimeView timeView;
+
+    
+    void Start()
+    {
+        timeModel.TotalTime.Subscribe(x => timeView.DisplayTime(x)).AddTo(this);
+
+        timeModel.TimerAsync(this.GetCancellationTokenOnDestroy()).Forget();
+    }
+}
---
+++ Assets/Scripts/UI/TimeView.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class TimeView : MonoBehaviour
+{
+    [SerializeField]
+    private Text txtTime;
+
+    [SerializeField]
+    private TMP_Text tmpTime;
+
+    [SerializeField]
+    private Ease ease;
+
+    private int oldTime = 0;
+    private float duration = 0.5f;
+
+
+    public void DisplayTime(int time) {
+        //txtTime.text = time.ToString();
+
+       ///tmpTime.text = time.ToString();
+
+        txtTime.DOCounter(oldTime, time, duration).SetEase(Ease.Linear);
+
+        tmpTime.DOCounter(oldTime, time, duration).SetEase(ease);
+        oldTime = time;
+    }
+}
